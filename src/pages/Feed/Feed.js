47a0da86ae4f1836d7b34fb3324aa774b7dc2cca
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-06-23 15:57:04 +0300

    added status logic

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,7 +22,12 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status',{
+      method:'GET',
+      headers: {
+        Authorization: "Bearer " + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -75,7 +80,14 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status',{
+      method:'PATCH',
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({status: this.state.status})
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
